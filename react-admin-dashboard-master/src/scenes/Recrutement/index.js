9544f2863e345f23f5be15fbb2f4a7145fc7bbc5
CHAOUCH Rim <rim.chaouch@soprahr.com>
2024-08-12 13:17:13 +0200

    Final solution

--- react-admin-dashboard-master/src/scenes/Recrutement/index.js
+++ react-admin-dashboard-master/src/scenes/Recrutement/index.js
@@ -8,7 +8,7 @@ const  Recrutement = () => {
   return (
     <Box m="2px">
       {/* HEADER */}
-      <Box  display="flex" justifyContent="center" alignItems="center" mb="20px">
+      <Box  display="flex" justifyContent="center" alignItems="center" >
         <Typography variant="h3" >
         Vue globale de recrutement de l'années courante
         </Typography>
@@ -18,7 +18,7 @@ const  Recrutement = () => {
       <Box  display="flex" justifyContent="center" >
         <iframe
         src="http://localhost:4848/single/?appid=C%3A%5CUsers%5Crchaouch%5CDocuments%5CQlik%5CSense%5CApps%5CRim_Chaouch_UI(1)(2).qvf&sheet=29823c05-4811-4b20-9f44-e2b1daf23995&theme=card&opt=ctxmenu"
-        scrolling="none"  style={{ overflow:"hidden", border: "none", width: "1120px", height: "630px" }}
+        scrolling="none"  style={{ overflow:"hidden", border: "none", width: "1120px", height: "590px" }}
         ></iframe>
       </Box>
     </Box>
